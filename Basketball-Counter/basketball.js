c039d96aee77949cad2c7ce58f44a793a75e1c97
Enriquepk <epintokilic@gmail.com>
2025-07-12 18:14:36 +0200

    javascript & centering with css

---
+++ Basketball-Counter/basketball.js
@@ -0,0 +1,34 @@
+let countHome = 0
+let countGuest = 0
+let countElHome = document.getElementById("count-el-home")
+let countElGuest = document.getElementById("count-el-guest")
+
+function increment1home() {
+    countHome += + 1
+    countElHome.textContent = countHome
+}
+
+function increment2home() {
+    countHome += + 2
+    countElHome.textContent = countHome
+}
+
+function increment3home() {
+    countHome += + 3
+    countElHome.textContent = countHome
+}
+
+function increment1guest() {
+    countGuest += + 1
+    countElGuest.textContent = countGuest
+}
+
+function increment2guest() {
+    countGuest += + 2
+    countElGuest.textContent = countGuest
+}
+
+function increment3guest() {
+    countGuest += + 3
+    countElGuest.textContent = countGuest
+}
