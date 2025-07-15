4c516bff1780e43d80d819e4a0d267712d26bc27
Enriquepk <epintokilic@gmail.com>
2025-07-15 16:31:31 +0200

    JavaScript finished

--- Password-Generator/password.js
+++ Password-Generator/password.js
@@ -1,2 +1,113 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password1El = document.getElementById("password1-el");
+let password2El = document.getElementById("password2-el");
+
+
+function generatePasswords() {
+    let length = 15
+    let password1 = ""
+    let password2 = ""
+
+    for (let i = 0; i < length; i++) {
+        let randomChar1 = characters[Math.floor(Math.random() * characters.length)]
+        let randomChar2 = characters[Math.floor(Math.random() * characters.length)]
+        password1 += randomChar1
+        password2 += randomChar2
+    }
+
+    password1El.textContent = password1
+    password2El.textContent = password2
+}
